82781e599dcdbf12b690410a22b1994c1e6a11b6
unknown <analistasistemasodontofamily@gmail.com>
2023-04-28 11:00:09 -0500

    agenda1 curso2

---
+++ funciones.js
@@ -0,0 +1,11 @@
+function doGet()
+{
+    return HtmlService.createTemplateFromFile('web').evaluate();
+ 
+
+}
+
+function obtenerDatosHTML (nombre){
+ return HtmlService.createHtmlOutputFromFile(nombre).getContent();
+
+}
